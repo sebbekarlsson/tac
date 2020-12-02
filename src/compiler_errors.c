780edf1ae3518482b0ddbb377e7fcb3d9cc2241a
Sami Alameddine <68682150+alsamitech@users.noreply.github.com>
2020-12-02 02:38:12 -0600

    Added better error highligting!

--- src/compiler_errors.c
+++ src/compiler_errors.c
@@ -8,8 +8,8 @@ void assert_call_matches_signature(AST_T* call_node, AST_T* func_node)
 
   if (actual_nr_args < expected_nr_args)
   {
-    printf(
-      "[Compilation Error]: `%s` expected %d arguments bot got %d.\n",
+    fprintf(stderr,
+      "[Compilation Error]: `%s` expected \033[1;32m%d\033[0m arguments bot got \033[1;31m%d\033[0m.\n",
       func_node->name,
       expected_nr_args,
       actual_nr_args
